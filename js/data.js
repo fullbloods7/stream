6ead7a2f0ab1e2816a916119cb23cc224c5cb2d9
fullbloods7 <fullbloods784@gmail.com>
2026-01-11 14:25:53 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -10,12 +10,80 @@ const videos = [
       {
         title: "Bade Achhe Lagte Hai Part-1",
         thumb: "assets/thumbnails/1.jpg",
-        embed: "https://streamtape.com/e/XXXX1"
+        embed: "https://streamtape.com/e/0eoYBXrXWmibbLA/"
       },
       {
         title: "Bade Achhe Lagte Hai Part-2",
         thumb: "assets/thumbnails/2.jpg",
-        embed: "https://streamtape.com/e/0eoYBXrXWmibbLA/"
+        embed: "https://streamtape.com/e/G9mdm2DgrjF1LkJ/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "tapan",
+    name: "Tapan S1",
+    category: "Ullu",
+    thumb: "assets/thumbnails/3.jpg",
+
+    episodes: [
+      {
+        title: "Tapan S1 Part-1",
+        thumb: "assets/thumbnails/3.jpg",
+        embed: "https://streamtape.com/e/VmkgOewkqvFk0z/"
+      },
+      {
+        title: "Tapan S1 Part-2",
+        thumb: "assets/thumbnails/4.jpg",
+        embed: "https://streamtape.com/e/VmkgOewkqvFk0z/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "anni",
+    name: "Anniversary Gift",
+    category: "Ullu",
+    thumb: "assets/thumbnails/5.jpg",
+
+    episodes: [
+      {
+        title: "Anniversary Gift Part-1",
+        thumb: "assets/thumbnails/6.jpg",
+        embed: "https://streamtape.com/e/A1V2MwM98aTX8wZ/"
+      },
+      {
+        title: "Anniversary Gift Part-2",
+        thumb: "assets/thumbnails/6.jpg",
+        embed: "https://streamtape.com/e/49Pdd7lrX3cVPZ/"
+      },
+      {
+        title: "Anniversary Gift Part-3",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/egyxvQmr1oIYRw6/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "care",
+    name: "Caretaker S1",
+    category: "Ullu",
+    thumb: "assets/thumbnails/8.jpg",
+
+    episodes: [
+      {
+        title: "Caretaker S1 Part-1",
+        thumb: "assets/thumbnails/8.jpg",
+        embed: "https://streamtape.com/e/WPb7mR2AG8T0Jk/"
+      },
+      {
+        title: "Caretaker S1 Part-2",
+        thumb: "assets/thumbnails/9.jpg",
+        embed: "https://streamtape.com/e/yBkJprMmxJi1Z7g/"
       }
     ]
   },
