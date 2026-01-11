f773ed466b1fc88bf090dcf2a34747fc5369d47b
fullbloods7 <fullbloods784@gmail.com>
2026-01-11 19:37:34 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -46,12 +46,12 @@ const videos = [
     id: "anni",
     name: "Anniversary Gift",
     category: "Ullu",
-    thumb: "assets/thumbnails/5.jpg",
+    thumb: "assets/thumbnails/7.jpg",
 
     episodes: [
       {
         title: "Anniversary Gift Part-1",
-        thumb: "assets/thumbnails/6.jpg",
+        thumb: "assets/thumbnails/5.jpg",
         embed: "https://streamtape.com/e/A1V2MwM98aTX8wZ/"
       },
       {
@@ -72,17 +72,17 @@ const videos = [
     id: "care",
     name: "Caretaker S1",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/8.jpg",
 
     episodes: [
       {
         title: "Caretaker S1 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/8.jpg",
         embed: "https://streamtape.com/e/WPb7mR2AG8T0Jk/"
       },
       {
         title: "Caretaker S1 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/8.jpg",
         embed: "https://streamtape.com/e/yBkJprMmxJi1Z7g/"
       }
     ]
@@ -93,12 +93,12 @@ const videos = [
     id: "black",
     name: "Blackmail S1",
     category: "Ullu",
-    thumb: "assets/thumbnails/1.jpg",
+    thumb: "assets/thumbnails/9.jpg",
 
     episodes: [
       {
         title: "Blackmail S1 Part-1",
-        thumb: "assets/thumbnails/1.jpg",
+        thumb: "assets/thumbnails/9.jpg",
         embed: "https://streamtape.com/e/Aw0G1Pr1rWUX673/"
       }
     ]
@@ -109,12 +109,12 @@ const videos = [
     id: "MrsTeacher",
     name: "Mrs Teacher (2023) S01",
     category: "PrimeShots",
-    thumb: "assets/thumbnails/2.jpg",
+    thumb: "assets/thumbnails/38.jpg",
 
     episodes: [
       {
         title: "Mrs Teacher (2023) S01 Part-1",
-        thumb: "assets/thumbnails/2.jpg",
+        thumb: "assets/thumbnails/38.jpg",
         embed: "https://streamtape.com/e/7Rpw3q184yioxO/"
       }
     ]
@@ -125,17 +125,17 @@ const videos = [
     id: "Khilona",
     name: "Khilona (2025) S01",
     category: "Ullu",
-    thumb: "assets/thumbnails/3.jpg",
+    thumb: "assets/thumbnails/11.jpg",
 
     episodes: [
       {
         title: "Khilona (2025) S01 Part-1",
-        thumb: "assets/thumbnails/3.jpg",
+        thumb: "assets/thumbnails/10.jpg",
         embed: "https://streamtape.com/e/m1MQaOqBzxFbPkG/"
       },
       {
         title: "Khilona (2025) S01 Part-2",
-        thumb: "assets/thumbnails/3.jpg",
+        thumb: "assets/thumbnails/11.jpg",
         embed: "https://streamtape.com/e/B71XDrVpl3SKjq/"
       }
     ]
@@ -146,7 +146,7 @@ const videos = [
     id: "Talab",
     name: "Talab (2025) S01",
     category: "Ullu",
-    thumb: "assets/thumbnails/5.jpg",
+    thumb: "assets/thumbnails/12.jpg",
 
     episodes: [
       {
@@ -156,7 +156,7 @@ const videos = [
       },
       {
         title: "Talab (2025) S01 Part-2",
-        thumb: "assets/thumbnails/6.jpg",
+        thumb: "assets/thumbnails/12.jpg",
         embed: "https://streamtape.com/e/QX3q9DgvApt0xQZ/"
       }
     ]
@@ -167,12 +167,12 @@ const videos = [
     id: "Paglet",
     name: "Paglet (2022) S01",
     category: "Paglet",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/40.jpg",
 
     episodes: [
       {
         title: "Paglet (2022) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/40.jpg",
         embed: "https://streamtape.com/e/WG4zkAZmr9TeBx/"
       }
     ]
@@ -183,17 +183,17 @@ const videos = [
     id: "Paglet2",
     name: "Paglet (2022) S02",
     category: "Paglet",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/41.jpg",
 
     episodes: [
       {
         title: "Paglet (2022) S02 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/41.jpg",
         embed: "https://streamtape.com/e/Ky47ALA1e3c0dP8/"
       },
       {
         title: "Paglet (2022) S02 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/42.jpg",
         embed: "https://streamtape.com/e/8Vk7yKwpv9toKkk/"
       }
     ]
@@ -204,17 +204,17 @@ const videos = [
     id: "Paglet3",
     name: "Paglet (2022) S03",
     category: "Paglet",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/44.jpg",
 
     episodes: [
       {
         title: "Paglet (2022) S03 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/43.jpg",
         embed: "https://streamtape.com/e/7GDkRpV9e6HoRD/"
       },
       {
         title: "Paglet (2022) S03 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/44.jpg",
         embed: "https://streamtape.com/e/vKw90YjkdGC419y/"
       }
     ]
@@ -225,17 +225,17 @@ const videos = [
     id: "Paglet4",
     name: "Paglet (2024) S04",
     category: "Paglet",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/45.jpg",
 
     episodes: [
       {
         title: "Paglet (2024) S04 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/45.jpg",
         embed: "https://streamtape.com/e/3rkbarGVV6fdbXR/"
       },
       {
         title: "Paglet (2024) S04 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/45.jpg",
         embed: "https://streamtape.com/e/kKqdy20qyPUOxgw/"
       }
     ]
@@ -246,12 +246,12 @@ const videos = [
     id: "Pyasi",
     name: "Pyasi Jawani (2025) S01",
     category: "Other",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/46.jpg",
 
     episodes: [
       {
         title: "Pyasi Jawani (2025) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/46.jpg",
         embed: "https://streamtape.com/e/Ok7eDGmLWJTZpKZ/"
       }
     ]
@@ -262,12 +262,12 @@ const videos = [
     id: "Seal5",
     name: "Seal 5 (2023) S05",
     category: "PrimeShots",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/39.jpg",
 
     episodes: [
       {
         title: "Seal 5 (2023) S05 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/39.jpg",
         embed: "https://streamtape.com/e/kWQrJ9BeLYuQ7L/"
       }
     ]
@@ -278,17 +278,17 @@ const videos = [
     id: "ceo",
     name: "C. E. O (2025) S01",
     category: "Atrangii",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/55.jpg",
 
     episodes: [
       {
         title: "C. E. O (2025) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/54.jpg",
         embed: "https://streamtape.com/e/vpodwPqWXAT4vlx/"
       },
       {
         title: "C. E. O (2025) S01 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/55.jpg",
         embed: "https://streamtape.com/e/1jo3dgMLjKsejQ7/"
       }
     ]
@@ -299,12 +299,12 @@ const videos = [
     id: "Kasturi",
     name: "Kasturi (2025) S01",
     category: "Other",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/47.jpg",
 
     episodes: [
       {
         title: "Kasturi (2025) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/47.jpg",
         embed: "https://streamtape.com/e/39Q0mv4KZDtdbMo/"
       }
     ]
@@ -315,12 +315,12 @@ const videos = [
     id: "RoomNo69",
     name: "Room No 69 (2025) S01",
     category: "Other",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/48.jpg",
 
     episodes: [
       {
         title: "Room No 69 (2025) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/48.jpg",
         embed: "https://streamtape.com/e/kKMm7RAY2xfOxzm/"
       }
     ]
@@ -331,17 +331,17 @@ const videos = [
     id: "Chhu",
     name: "Chhu Le Zara (2025) S01",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/13.jpg",
 
     episodes: [
       {
         title: "Chhu Le Zara (2025) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/13.jpg",
         embed: "https://streamtape.com/e/8d6VYkmPJmHow33/"
       },
       {
         title: "Chhu Le Zara (2025) S01 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/14.jpg",
         embed: "https://streamtape.com/e/dRKzd8p3LlTZvG/"
       }
     ]
@@ -352,12 +352,12 @@ const videos = [
     id: "Bhabhi420",
     name: "Bhabhi 420 (2025) S01",
     category: "Other",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/49.jpg",
 
     episodes: [
       {
         title: "Bhabhi 420 (2025) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/49.jpg",
         embed: "https://streamtape.com/e/bPGkpOj1YXHQZy/"
       }
     ]
@@ -368,17 +368,17 @@ const videos = [
     id: "TuDekh",
     name: "Tu Dekh Meri Photo (2025)",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/15.jpg",
 
     episodes: [
       {
         title: "Tu Dekh Meri Photo (2025) Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/15.jpg",
         embed: "https://streamtape.com/e/xqDWVjv2ZZUvWl/"
       },
       {
         title: "Tu Dekh Meri Photo (2025) Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/16.jpg",
         embed: "https://streamtape.com/e/6R48J4VJQbh92Gr/"
       }
     ]
@@ -389,22 +389,22 @@ const videos = [
     id: "ChummaChaat",
     name: "Chumma Chaati (2025) S01",
     category: "Other",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/51.jpg",
 
     episodes: [
       {
         title: "Chumma Chaati (2025) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/50.jpg",
         embed: "https://streamtape.com/e/arq0jM4wO6Ixw3d/"
       },
       {
         title: "Chumma Chaati (2025) S01 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/51.jpg",
         embed: "https://streamtape.com/e/pDGrvk8VoKiG74/"
       },
       {
         title: "Chumma Chaati (2025) S01 Part-3",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/52.jpg",
         embed: "https://streamtape.com/e/qy86MkDGMeuaGy/"
       }
     ]
@@ -415,7 +415,7 @@ const videos = [
     id: "Chuski",
     name: "Chuski (2024) S01",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/17.jpg",
 
     episodes: [
       {
@@ -425,7 +425,7 @@ const videos = [
       },
       {
         title: "Chuski (2024) S01 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/17.jpg",
         embed: "https://streamtape.com/e/wDaOpp1DX4HJ0p9/"
       }
     ]
@@ -436,22 +436,22 @@ const videos = [
     id: "Chawl",
     name: "Chawl House (2021) S01",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/18.jpg",
 
     episodes: [
       {
         title: "Chawl House (2021) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/18.jpg",
         embed: "https://streamtape.com/e/kgLjQlYl8YIDBg/"
       },
       {
         title: "Chawl House (2021) S01 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/19.jpg",
         embed: "https://streamtape.com/e/kZ7DkV2b6XfDLx/"
       },
       {
         title: "Chawl House (2021) S01 Part-3",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/20.jpg",
         embed: "https://streamtape.com/e/xkVx6oBByeFk3jz/"
       }
     ]
@@ -462,22 +462,22 @@ const videos = [
     id: "Sursuri",
     name: "Sursuri-Li (2022) S01",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/22.jpg",
 
     episodes: [
       {
         title: "Sursuri-Li (2022) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/23.jpg",
         embed: "https://streamtape.com/e/oxBXjkv4PbcJVmV/"
       },
       {
         title: "Sursuri-Li (2022) S01 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/22.jpg",
         embed: "https://streamtape.com/e/JYGDwdZX43sjW6y/"
       },
       {
         title: "Sursuri-Li (2022) S01 Part-3",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/21.jpg",
         embed: "https://streamtape.com/e/OqwZ7lWxlXfZvQa/"
       }
     ]
@@ -488,17 +488,17 @@ const videos = [
     id: "Aadhi",
     name: "Aadhi Gharwali (2025) S01",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/24.jpg",
 
     episodes: [
       {
         title: "Aadhi Gharwali (2025) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/24.jpg",
         embed: "https://streamtape.com/e/V0a8Z09POQTKVj2/"
       },
       {
         title: "Aadhi Gharwali (2025) S01 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/25.jpg",
         embed: "https://streamtape.com/e/3JoAjmvXqzuX28/"
       }
     ]
@@ -509,17 +509,17 @@ const videos = [
     id: "KhoonBhari",
     name: "Khoon Bhari Maang (2022) S01",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/26.jpg",
 
     episodes: [
       {
         title: "Khoon Bhari Maang (2022) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/26.jpg",
         embed: "https://streamtape.com/e/lGXgeOr8m4fzjx/"
       },
       {
         title: "Khoon Bhari Maang (2022) S01 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/27.jpg",
         embed: "https://streamtape.com/e/x1L3KzYlKvSkAMO/"
       }
     ]
@@ -530,12 +530,12 @@ const videos = [
     id: "Jaal",
     name: "Jaal (2022) S01 Complete",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/28.jpg",
 
     episodes: [
       {
         title: "Jaal (2022) S01 Complete Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/28.jpg",
         embed: "https://streamtape.com/e/7zL8Q2d2lWhA7v8/"
       }
     ]
@@ -546,22 +546,22 @@ const videos = [
     id: "Badan",
     name: "Badan (2023) S01",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/29.jpg",
 
     episodes: [
       {
         title: "Badan (2023) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/29.jpg",
         embed: "https://streamtape.com/e/Wbjj73DP3QtAo4/"
       },
       {
         title: "Badan (2023) S01 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/30.jpg",
         embed: "https://streamtape.com/e/mrZ09pPyMbsb0b8/"
       },
       {
         title: "Badan (2023) S01 Part-3",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/31.jpg",
         embed: "https://streamtape.com/e/lWb4z8oOGBC7Xlq/"
       }
     ]
@@ -572,17 +572,17 @@ const videos = [
     id: "MereAngane",
     name: "Mere Angane Mein (2025) S01",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/32.jpg",
 
     episodes: [
       {
         title: "Mere Angane Mein (2025) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/32.jpg",
         embed: "https://streamtape.com/e/BkK02l3ZvdhKAP/"
       },
       {
         title: "Mere Angane Mein (2025) S01 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/32.jpg",
         embed: "https://streamtape.com/e/9bMDLWKlGWFaJ6m/"
       }
     ]
@@ -593,22 +593,22 @@ const videos = [
     id: "Watchman",
     name: "Watchman (2023) S01",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/33.jpg",
 
     episodes: [
       {
         title: "Watchman (2023) S01 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/33.jpg",
         embed: "https://streamtape.com/e/Vr0yKg22pOTK17M/"
       },
       {
         title: "Watchman (2023) S01 Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/34.jpg",
         embed: "https://streamtape.com/e/A0KBJlOzeesWrR/"
       },
       {
         title: "Watchman (2023) S01 Part-3",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/35.jpg",
         embed: "https://streamtape.com/e/K9ZJ6vXkb2TJqP/"
       }
     ]
@@ -619,17 +619,17 @@ const videos = [
     id: "DilSambhal",
     name: "Dil Sambhal Ja Zara (2024)",
     category: "Ullu",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/37.jpg",
 
     episodes: [
       {
         title: "Dil Sambhal Ja Zara (2024) Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/36.jpg",
         embed: "https://streamtape.com/e/wa02926YlXSxZ8/"
       },
       {
         title: "Dil Sambhal Ja Zara (2024) Part-2",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/37.jpg",
         embed: "https://streamtape.com/e/R7wLWeOG1jtJzr/"
       }
     ]
@@ -640,12 +640,12 @@ const videos = [
     id: "SarlaBhabhi",
     name: "Sarla Bhabhi (2020) S04",
     category: "Other",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/53.jpg",
 
     episodes: [
       {
         title: "Sarla Bhabhi (2020) S04 Part-1",
-        thumb: "assets/thumbnails/7.jpg",
+        thumb: "assets/thumbnails/53.jpg",
         embed: "https://streamtape.com/e/1WL72rwD4yTem94/"
       }
     ]
@@ -656,7 +656,7 @@ const videos = [
     id: "ChristmasPart3",
     name: "The Three Fucks Of Christmas Part-3 (2025)",
     category: "Porn",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/56.jpg",
     embed: "https://streamtape.com/e/9R8AKYzYWxFa0gq/"
   },
 
@@ -665,7 +665,7 @@ const videos = [
     id: "SuperExpression",
     name: "Super Expression (2025) MomXXX",
     category: "Porn",
-    thumb: "assets/thumbnails/7.jpg",
+    thumb: "assets/thumbnails/57.jpg",
     embed: "https://streamtape.com/e/rKvrXXkbeKib2LJ/"
   }
 ];
