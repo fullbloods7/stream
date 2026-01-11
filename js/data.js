239f2490c11186ef140610dab2028a0dc255919a
fullbloods7 <fullbloods784@gmail.com>
2026-01-11 18:30:08 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -36,7 +36,7 @@ const videos = [
       {
         title: "Tapan S1 Part-2",
         thumb: "assets/thumbnails/4.jpg",
-        embed: "https://streamtape.com/e/VmkgOewkqvFk0z/"
+        embed: "https://streamtape.com/e/jvydgJ8lRQHQ3X/"
       }
     ]
   },
@@ -72,29 +72,601 @@ const videos = [
     id: "care",
     name: "Caretaker S1",
     category: "Ullu",
-    thumb: "assets/thumbnails/8.jpg",
+    thumb: "assets/thumbnails/7.jpg",
 
     episodes: [
       {
         title: "Caretaker S1 Part-1",
-        thumb: "assets/thumbnails/8.jpg",
+        thumb: "assets/thumbnails/7.jpg",
         embed: "https://streamtape.com/e/WPb7mR2AG8T0Jk/"
       },
       {
         title: "Caretaker S1 Part-2",
-        thumb: "assets/thumbnails/9.jpg",
+        thumb: "assets/thumbnails/7.jpg",
         embed: "https://streamtape.com/e/yBkJprMmxJi1Z7g/"
       }
     ]
   },
 
   {
+    type: "series",
+    id: "black",
+    name: "Blackmail S1",
+    category: "Ullu",
+    thumb: "assets/thumbnails/1.jpg",
+
+    episodes: [
+      {
+        title: "Blackmail S1 Part-1",
+        thumb: "assets/thumbnails/1.jpg",
+        embed: "https://streamtape.com/e/Aw0G1Pr1rWUX673/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "MrsTeacher",
+    name: "Mrs Teacher (2023) S01",
+    category: "PrimeShots",
+    thumb: "assets/thumbnails/2.jpg",
+
+    episodes: [
+      {
+        title: "Mrs Teacher (2023) S01 Part-1",
+        thumb: "assets/thumbnails/2.jpg",
+        embed: "https://streamtape.com/e/7Rpw3q184yioxO/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Khilona",
+    name: "Khilona (2025) S01",
+    category: "Ullu",
+    thumb: "assets/thumbnails/3.jpg",
+
+    episodes: [
+      {
+        title: "Khilona (2025) S01 Part-1",
+        thumb: "assets/thumbnails/3.jpg",
+        embed: "https://streamtape.com/e/m1MQaOqBzxFbPkG/"
+      },
+      {
+        title: "Khilona (2025) S01 Part-2",
+        thumb: "assets/thumbnails/3.jpg",
+        embed: "https://streamtape.com/e/B71XDrVpl3SKjq/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Talab",
+    name: "Talab (2025) S01",
+    category: "Ullu",
+    thumb: "assets/thumbnails/5.jpg",
+
+    episodes: [
+      {
+        title: "Talab (2025) S01 Part-1",
+        thumb: "assets/thumbnails/.jpg",
+        embed: ""
+      },
+      {
+        title: "Talab (2025) S01 Part-2",
+        thumb: "assets/thumbnails/6.jpg",
+        embed: "https://streamtape.com/e/QX3q9DgvApt0xQZ/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Paglet",
+    name: "Paglet (2022) S01",
+    category: "Paglet",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Paglet (2022) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/WG4zkAZmr9TeBx/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Paglet2",
+    name: "Paglet (2022) S02",
+    category: "Paglet",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Paglet (2022) S02 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/Ky47ALA1e3c0dP8/"
+      },
+      {
+        title: "Paglet (2022) S02 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/8Vk7yKwpv9toKkk/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Paglet3",
+    name: "Paglet (2022) S03",
+    category: "Paglet",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Paglet (2022) S03 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/7GDkRpV9e6HoRD/"
+      },
+      {
+        title: "Paglet (2022) S03 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/vKw90YjkdGC419y/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Paglet4",
+    name: "Paglet (2024) S04",
+    category: "Paglet",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Paglet (2024) S04 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/3rkbarGVV6fdbXR/"
+      },
+      {
+        title: "Paglet (2024) S04 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/kKqdy20qyPUOxgw/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Pyasi",
+    name: "Pyasi Jawani (2025) S01",
+    category: "Other",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Pyasi Jawani (2025) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/Ok7eDGmLWJTZpKZ/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Seal5",
+    name: "Seal 5 (2023) S05",
+    category: "PrimeShots",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Seal 5 (2023) S05 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/kWQrJ9BeLYuQ7L/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "ceo",
+    name: "C. E. O (2025) S01",
+    category: "Atrangii",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "C. E. O (2025) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/vpodwPqWXAT4vlx/"
+      },
+      {
+        title: "C. E. O (2025) S01 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/1jo3dgMLjKsejQ7/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Kasturi",
+    name: "Kasturi (2025) S01",
+    category: "Other",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Kasturi (2025) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/39Q0mv4KZDtdbMo/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "RoomNo69",
+    name: "Room No 69 (2025) S01",
+    category: "Other",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Room No 69 (2025) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/kKMm7RAY2xfOxzm/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Chhu",
+    name: "Chhu Le Zara (2025) S01",
+    category: "Ullu",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Chhu Le Zara (2025) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/8d6VYkmPJmHow33/"
+      },
+      {
+        title: "Chhu Le Zara (2025) S01 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/dRKzd8p3LlTZvG/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Bhabhi420",
+    name: "Bhabhi 420 (2025) S01",
+    category: "Other",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Bhabhi 420 (2025) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/bPGkpOj1YXHQZy/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "TuDekh",
+    name: "Tu Dekh Meri Photo (2025)",
+    category: "Ullu",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Tu Dekh Meri Photo (2025) Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/xqDWVjv2ZZUvWl/"
+      },
+      {
+        title: "Tu Dekh Meri Photo (2025) Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/6R48J4VJQbh92Gr/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "ChummaChaat",
+    name: "Chumma Chaati (2025) S01",
+    category: "Other",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Chumma Chaati (2025) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/arq0jM4wO6Ixw3d/"
+      },
+      {
+        title: "Chumma Chaati (2025) S01 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/pDGrvk8VoKiG74/"
+      },
+      {
+        title: "Chumma Chaati (2025) S01 Part-3",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/qy86MkDGMeuaGy/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Chuski",
+    name: "Chuski (2024) S01",
+    category: "Ullu",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Chuski (2024) S01 Part-1",
+        thumb: "assets/thumbnails/.jpg",
+        embed: ""
+      },
+      {
+        title: "Chuski (2024) S01 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/wDaOpp1DX4HJ0p9/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Chawl",
+    name: "Chawl House (2021) S01",
+    category: "Ullu",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Chawl House (2021) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/kgLjQlYl8YIDBg/"
+      },
+      {
+        title: "Chawl House (2021) S01 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/kZ7DkV2b6XfDLx/"
+      },
+      {
+        title: "Chawl House (2021) S01 Part-3",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/xkVx6oBByeFk3jz/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Sursuri",
+    name: "Sursuri-Li (2022) S01",
+    category: "Ullu",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Sursuri-Li (2022) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/oxBXjkv4PbcJVmV/"
+      },
+      {
+        title: "Sursuri-Li (2022) S01 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/JYGDwdZX43sjW6y/"
+      },
+      {
+        title: "Sursuri-Li (2022) S01 Part-3",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/OqwZ7lWxlXfZvQa/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Aadhi",
+    name: "Aadhi Gharwali (2025) S01",
+    category: "Ullu",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Aadhi Gharwali (2025) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/V0a8Z09POQTKVj2/"
+      },
+      {
+        title: "Aadhi Gharwali (2025) S01 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/3JoAjmvXqzuX28/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "KhoonBhari",
+    name: "Khoon Bhari Maang (2022) S01",
+    category: "Ullu",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Khoon Bhari Maang (2022) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/lGXgeOr8m4fzjx/"
+      },
+      {
+        title: "Khoon Bhari Maang (2022) S01 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/x1L3KzYlKvSkAMO/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Jaal",
+    name: "Jaal (2022) S01 Complete",
+    category: "Ullu",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Jaal (2022) S01 Complete Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/7zL8Q2d2lWhA7v8/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Badan",
+    name: "Badan (2023) S01",
+    category: "Ullu",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Badan (2023) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/Wbjj73DP3QtAo4/"
+      },
+      {
+        title: "Badan (2023) S01 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/mrZ09pPyMbsb0b8/"
+      },
+      {
+        title: "Badan (2023) S01 Part-3",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/lWb4z8oOGBC7Xlq/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "MereAngane",
+    name: "Mere Angane Mein (2025) S01",
+    category: "Ullu",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Mere Angane Mein (2025) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/BkK02l3ZvdhKAP/"
+      },
+      {
+        title: "Mere Angane Mein (2025) S01 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/9bMDLWKlGWFaJ6m/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "Watchman",
+    name: "Watchman (2023) S01",
+    category: "Ullu",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Watchman (2023) S01 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/Vr0yKg22pOTK17M/"
+      },
+      {
+        title: "Watchman (2023) S01 Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/A0KBJlOzeesWrR/"
+      },
+      {
+        title: "Watchman (2023) S01 Part-3",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/K9ZJ6vXkb2TJqP/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "DilSambhal",
+    name: "Dil Sambhal Ja Zara (2024)",
+    category: "Ullu",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Dil Sambhal Ja Zara (2024) Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/wa02926YlXSxZ8/"
+      },
+      {
+        title: "Dil Sambhal Ja Zara (2024) Part-2",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/R7wLWeOG1jtJzr/"
+      }
+    ]
+  },
+
+  {
+    type: "series",
+    id: "SarlaBhabhi",
+    name: "Sarla Bhabhi (2020) S04",
+    category: "Other",
+    thumb: "assets/thumbnails/7.jpg",
+
+    episodes: [
+      {
+        title: "Sarla Bhabhi (2020) S04 Part-1",
+        thumb: "assets/thumbnails/7.jpg",
+        embed: "https://streamtape.com/e/1WL72rwD4yTem94/"
+      }
+    ]
+  },
+
+  {
     type: "movie",
-    id: "rangeela",
-    name: "Rangeela Part-1",
-    category: "Movies",
-    thumb: "assets/thumbnails/rangeela.jpg",
-    embed: "https://streamtape.com/e/XXXX1"
+    id: "ChristmasPart3",
+    name: "The Three Fucks Of Christmas Part-3 (2025)",
+    category: "Porn",
+    thumb: "assets/thumbnails/7.jpg",
+    embed: "https://streamtape.com/e/9R8AKYzYWxFa0gq/"
+  },
+
+  {
+    type: "movie",
+    id: "SuperExpression",
+    name: "Super Expression (2025) MomXXX",
+    category: "Porn",
+    thumb: "assets/thumbnails/7.jpg",
+    embed: "https://streamtape.com/e/rKvrXXkbeKib2LJ/"
   }
 ];
 
